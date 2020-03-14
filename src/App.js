dc646c791ca7d9261298cb086544b973bd29950f
Wilkrest <jarirez@hotmail.com>
2020-03-14 01:17:41 -0500

    finished functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,95 @@
 import React from 'react';
 import './App.css';
+import MovieRow from './MovieRow.js'
+//dollar sign represents the jquery library
+import $ from 'jquery'
 
-function App() {
-  return (
-    <div className="App">
-      
-      <table className='title-bar'>
-        <tbody>
-          <tr>
-            <td>
-              <img width ="50" alt="movie logo" src="logo.svg"/>
-            </td>
-            <td width='8'></td>
-            <td>
-              <h1>  MoviesDB Search </h1>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+class App extends React.Component 
+{
 
-      <input className="main-search-bar" placeholder="Enter movie title..."></input>
+  //constructor for this component
+  constructor(props)
+  {
+    super(props)
+    //initialize state to an empty state
+    this.state = {}
+    //perform the search 
+    this.performSearch("")
+  }
 
-    </div>
+  //this function executes the search on movieDB and returns the results
+  performSearch(searchTerm) 
+  {
+    //jquery has a function called ajax used to make asynchronous calls to fetch data
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=98350ecd42c9cc6f0c3ca343d790f8e9&query=" + searchTerm
+    $.ajax({
+      //holds the url to fetch data from 
+      url: urlString,
+      //callback function executed on success
+      //searchResults is the fetched data, which should be in the form of an array of JSON objects
+      success: (searchResults) => 
+      {
+        console.log("fetched data successfully")
 
-  );
+        //save the fetched data into results, results will now be an array of objects
+        const results = searchResults.results
+        //initalize array to contain the constructed movie row components
+        const movieRows = []
+
+        //parse through the fetched JSON array building MovieRow components with each element in the fetched array
+        results.forEach((movie) => 
+        {
+          //construct the new MovieRow component with each 'movie' in the results array
+          const newMovie = <MovieRow key={movie.id} movie={movie} />
+          movieRows.push(newMovie)
+        })
+        
+        //set the current state
+        this.setState({rows: movieRows})
+
+      },
+      //callback function excecuted on error
+      error: (xhr, status, err) => {
+        console.error("failed to fetch data")
+      }
+
+    })
+  }
+
+  searchHandler(event) 
+  {
+    const searchTerm = event.target.value
+    this.performSearch(searchTerm)
+    
+  }
+
+
+  render() 
+  {
+    return (
+      <div className="App">
+        <table className='title-bar'>
+          <tbody>
+            <tr>
+              <td>
+                <img width ="50" alt="movie logo" src="logo.svg"/>
+              </td>
+              <td width='8'></td>
+              <td>
+                <h1>  MoviesDB Search </h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <input className="main-search-bar" onChange={this.searchHandler.bind(this)} placeholder="Enter movie title..."></input>
+  
+        {this.state.rows}    
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/MovieRow.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+
+//props are the arguements passed into the component form the parent component
+//movie is the name of the attribute passed through the props
+class MovieRow extends React.Component {
+
+    viewMovie() 
+    {
+        //build the link to the movie using the given prop's id
+        const url = "https://www.themoviedb.org/movie/" + this.props.movie.id
+        //this line redirects the user to the given link
+        window.location.href = url
+    }
+
+
+
+    render() {
+        const posterURL = 'https://image.tmdb.org/t/p/w185/' + this.props.movie.poster_path
+
+        return <table key={this.props.movie.id} className="movie-row">
+        <tbody> 
+            <tr>
+                <td className="movie-poster">
+                    <img alt="poster" width="120" src={posterURL}/>
+                </td>
+                <td className="movie-details">
+                    <h3>{this.props.movie.title}</h3>
+                    <p>{this.props.movie.overview}</p>
+                    <input type="button" onClick={this.viewMovie.bind(this)} value="View"></input>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+    }
+}
+
+export default MovieRow;
